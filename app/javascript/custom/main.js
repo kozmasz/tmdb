61662ad6da338250bc9841b8c2565545968fe998
Kozma Szilveszter <szilveszter.kozma@itron.com>
2022-08-16 19:07:50 +0200

    Dummy frontend

---
+++ app/javascript/custom/main.js
@@ -0,0 +1,10 @@
+// Array
+//   .from(document.querySelectorAll('.dropdown-menu li a'))
+//   .forEach(selector => {
+//     //Add an event listener for displaying the selected dropdown item
+//     selector.addEventListener("click", function(e) {
+//       let selectorButton = document.getElementById("media-type");
+//       selectorButton.textContent = this.text;
+//       selectorButton.value = this.getAttribute("value");
+//     });
+//   });
